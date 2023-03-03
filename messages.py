724aa4e37b1c3071c5ebb6ae411eb2a6d289dc9d
jdev082 <willsjayden2@gmail.com>
2023-03-02 21:51:10 -0600

    feat: features and improvements

--- messages.py
+++ messages.py
@@ -1,3 +1,3 @@
 no_perms = "Only bot admins can run this command!"
 
-help_msg = "admin only:\nsay <message>\nstop\n[LC] restart\ngive_tempadmin\nrun\n\nuser commands:\nabout\nhelp\nwhoami\nspam\nlines\nsystem\n[LC] cat\n[LC] quote\nlink"
+help_msg = "admin only:\nconfig\nstop\n[LC] restart\ngive_tempadmin\nrun\n\nuser commands:\nabout\nhelp\nwhoami\nspam\nlines\nsystem\n[LC] cat\n[LC] quote\nlink\nlist\n[LC] say"
